53e03357ce41b021c11bbf2567a5bac3255e2471
alialoulou96 <157611594+alialoulou96@users.noreply.github.com>
2024-01-24 17:13:29 +0100

    Update test.py

--- test.py
+++ test.py
@@ -61,7 +61,7 @@ def url(id,url_df):
 
 
 df = pd.read_csv("https://github.com/alialoulou96/test2/blob/master/id_information_mmsr.tsv", delimiter='\t')
-st.markdown(df['artist'])
+st.markdown(df.columns)
 # df_artist = df['artist'].values.tolist()
 # df_song = df['song'].values.tolist()
 
