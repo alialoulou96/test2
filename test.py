616e252866d919cc8658851d48f27797210f3e51
alialoulou96 <157611594+alialoulou96@users.noreply.github.com>
2024-01-24 16:55:32 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
+import subprocess
+import sys
+import streamlit as st
+import random
+import pandas as pd
+import numpy as np 
+from sklearn.preprocessing import StandardScaler
+import torch
+from sklearn.metrics.pairwise import cosine_similarity
 import streamlit as st
 st.markdown("**Retrieved genres:**")
